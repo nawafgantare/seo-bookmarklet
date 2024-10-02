23e6c46ad13ce3b37a35d13e8a45b93de3de8717
Nawaf Gantare <nawafgantare@gmail.com>
2024-10-02 21:21:44 +0530

    Update test.js

--- test.js
+++ test.js
@@ -26,13 +26,13 @@ function loadPopup() {
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="popupTitle">ALERT</h5>
+                    <h5 class="modal-title" id="popupTitle">Results</h5>
                     <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div class="modal-body">
-                    This is a dynamically loaded Bootstrap popup!
+                <div class="modal-body" id="modalBody">
+                    Loading...
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -46,13 +46,57 @@ function loadPopup() {
         let div = document.createElement('div');
         div.innerHTML = popupHTML;
         document.body.appendChild(div);
-
-        // Wait for Bootstrap JS to be fully loaded before showing the popup
-        setTimeout(() => {
-            var popup = new bootstrap.Modal(document.getElementById('customPopup'));
-            popup.show();
-        }, 500); // Adjust delay as needed
     }
+
+    // Show the modal
+    let popup = new bootstrap.Modal(document.getElementById('customPopup'));
+    
+    // Call functions to get results and display them
+    let results = getMetaInfo();
+    results += noindexCheck();
+    document.getElementById('modalBody').innerHTML = results;
+    popup.show();
+}
+
+// Function to gather and display meta tags
+function getMetaInfo() {
+    let metaTags = document.getElementsByTagName('meta');
+    let title = document.title;
+    let metaInfo = {};
+    metaInfo['Title'] = (title ? title : '<span style="color:red;">NOT FOUND</span>');
+
+    for (let i = 0; i < metaTags.length; i++) {
+        let metaTag = metaTags[i];
+        let name = metaTag.getAttribute('name') || metaTag.getAttribute('property');
+        let content = metaTag.getAttribute('content');
+        if (name) {
+            metaInfo[name.replace(/:/g, ' ')] = content || '<span style="color:red;">NOT FOUND</span>';
+        }
+    }
+
+    // Create output
+    let output = '<pre>';
+    for (let key in metaInfo) {
+        output += `<b>${key}:</b> ${metaInfo[key]}\n`;
+    }
+    output += '</pre>';
+    return output;
+}
+
+// Function to check for noindex meta tag
+function noindexCheck() {
+    let metaTags = document.getElementsByTagName('meta');
+    let noIndexPresent = false;
+
+    for (let i = 0; i < metaTags.length; i++) {
+        let metaTag = metaTags[i];
+        if (metaTag.getAttribute('name') === 'robots' && metaTag.getAttribute('content').includes('noindex')) {
+            noIndexPresent = true;
+            break;
+        }
+    }
+    
+    return `<h2 style="color: ${noIndexPresent ? 'red' : 'green'};">Noindex tag is ${noIndexPresent ? 'present' : 'NOT present'}</h2>`;
 }
 
 // Call the function to load the popup when this script is executed
